2fc0516554bd4a7245682f261a51e441e726c9e9
DylanBedetti <dylanbedetti@gmail.com>
2019-07-21 14:19:05 +0800

    first commit

---
+++ 1 Learning/javascript/main.js
@@ -0,0 +1,31 @@
+// alert();
+
+// function go(name, age){
+//     alert(name);
+//     alert(age);
+// }
+
+// // go("dylan", 22);
+// alert(typeof("dylan"))
+
+
+var myList = ['applges', 'oranges', 'bananas']
+
+// myList.forEach(function(value, index){
+//     console.log(value, index);
+// })
+
+// while do for
+
+// var time = 0;
+
+// while (time < 10) {
+//     console.log("item", time);
+//     time = time + 1
+// }
+
+for (var i =0; i < 10; i++){
+    console.log("i is: ", i);
+}
+
+console.log(myList.length)
