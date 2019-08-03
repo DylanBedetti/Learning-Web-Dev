9582df09747fe1c244f2f03efc66edaef3ea9b8d
unknown <Dylan Bedetti>
2019-08-03 22:39:09 +0800

    learning js

---
+++ Javascript/first.js
@@ -0,0 +1,23 @@
+// data types
+/*
+undefined, null, boolean, string, symbol, number, object
+
+null -> empty
+symbol -> immutible primitive value that is uniqu
+object -> key, value pairs
+*/
+
+var myName = "dylan"; // use throughout entire program
+
+let ourName = "Codecamp"; // only used within scope 
+
+const pi = 3.14; // variable that can never change
+
+// *****************************************************************
+
+var a;
+var b = 2;
+a = 7;
+a *= 2;
+a /= 2;
+console.log(a);
